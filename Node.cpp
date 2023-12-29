7144501522e9f8c33f21acdfebcdf0831ce17a48
Michael <michaelcalle14@gmail.com>
2023-12-29 00:17:58 -0500

    braceformat

--- Node.cpp
+++ Node.cpp
@@ -2,30 +2,30 @@
  * Created by Michael Calle, Allison Lee, Angus Hu on December 27, 2023
 */
 
-Node::Node():value_(0), bias_(0){
+Node::Node():value_(0), bias_(0) {
     edges_ = {};
 }
 
-Node::Node(double value, double bias, std::vector<double> edges):Node(){
+Node::Node(double value, double bias, std::vector<double> edges):Node() {
     value_ = value;
     bias_ = bias;
     edges_ = edges;
 }
 
-double Node::getValue() const{
+double Node::getValue() const {
     return value_;
 }
 
-double Node::getBias() const{
+double Node::getBias() const {
     return bias_;
 }
 
 //I forgot the correct syntax for this. Just have it return the array. I think you need to return a pointer.
-std::vector<double> Node::getEdges() const{ 
+std::vector<double> Node::getEdges() const { 
     return edges_;
 }
     
-void Node::printNode() const{ //Just prints out the information in the node
+void Node::printNode() const { //Just prints out the information in the node
     std::cout << "Value: "  << value_ << "\nBias: " << bias_ << "\nEdges: ";
     for (int i = 0; i < edges_.size(); i++){
         std::cout << edges_[i] << " ";
@@ -34,12 +34,12 @@ void Node::printNode() const{ //Just prints out the information in the node
 }
 
 //Mutator
-void Node::setValue(double value){
+void Node::setValue(double value) {
     value_ = value;
 }
-void Node::setBias(double bias){
+void Node::setBias(double bias) {
     bias_ = bias;
 }
-void Node::setEdge(std::vector<double> edges){
+void Node::setEdge(std::vector<double> edges) {
     edges_ = edges;
 }
