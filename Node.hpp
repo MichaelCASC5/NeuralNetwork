250038d07d2e955cd4d87f505f40e1849ddcfc00
Allimonae <128328789+Allimonae@users.noreply.github.com>
2023-12-28 19:26:10 -0500

    started Node.hpp

--- Node.hpp
+++ Node.hpp
@@ -1,3 +1,21 @@
 /**
  * Created by Michael Calle, Allison Lee, Angus Hu on December 27, 2023
 */
+
+#ifndef NEURAL_NETWORK_NODE_HPP
+#define NEURAL_NETWORK_NODE_HPP
+
+#include <iostream>
+#include <vector>
+
+class Node{
+private:
+    double value_;
+    std::vector<double> edges_;
+    int position_;
+    int layer_;
+public:
+    bool addNode();
+    bool removeNode();
+};
+#endif
