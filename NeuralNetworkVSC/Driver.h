9936e42b58689f28163099ecaac9e6cd73f1085a
Michael <michaelcalle14@gmail.com>
2024-08-23 19:29:40 -0400

    clean up

--- NeuralNetworkVSC/Driver.h
+++ NeuralNetworkVSC/Driver.h
@@ -3,19 +3,18 @@
 #ifndef DRIVER_HPP
 #define DRIVER_HPP
 
-// Classes include
-#include "Network.h"
-#include "Car.h"
-#include "Point.h"
-
 // C include
 #include <iostream>
 #include <thread>
 #include <mutex>
 #include <chrono>
 
+// Classes include
+#include "CarSim.h"
+
 // External library include
 #include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
 
 class Driver
 {
@@ -31,46 +30,48 @@ public:
     ~Driver();
 
     /**
+     * Game logic
+     */
+    void logic();
+
+    /**
+     * Game paint
+     */
+    void paint();
+
+    /**
+     * Handles mouse events
+     */
+    void mouseEvents();
+
+    /**
+     * Hosts the game logic
+     */
+    void logicContainer();
+
+    /**
+     * Hosts the painting
+     */
+    void paintContainer();
+
+    /**
      * Start the threaded game
      */
     void start();
-
-    /**
-     * Perform game logic
-     */
-    void loop();
-
-    /**
-     * Paint to screen
-     */
-    void paintComponent();
 
 private:
     /**
      * Threading and window variables
      */
     sf::RenderWindow window;
-    std::thread loopThread;
+    std::thread logicThread;
     std::mutex mutex;
-
-    // Program running boolean
     bool runProgram;
 
     /**
-     * GAME PRIVATE MEMBERS
+     * Simulation
      */
-    std::chrono::steady_clock::time_point startTimer;
-
-    std::vector<Car> cars;
-
-    std::vector<Point> obstacles;
-    bool obs[20][20] = {};
-
-    int startPoint[2] = { 50, 50 };
-    int end[2] = { 390, 390 };
-
-    //Generation counter
-    int generation = 0;
+    CarSim carSim;
 };
 
 #endif
--- NeuralNetworkVSC/main.cpp
+++ NeuralNetworkVSC/main.cpp
@@ -1,11 +1,9 @@
-#include "Network.h"
-#include "SFML/Graphics.hpp"
 #include "Driver.h"
 
 int main()
 {
 	Driver g;
 	g.start();
-
+	
 	return 0;
 }
